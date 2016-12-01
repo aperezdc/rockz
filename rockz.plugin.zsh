05faae8f26b6f8b1afa7ed8dd6e2b1f6c2d88734
Adrian Perez de Castro <aperez@igalia.com>
2016-12-01 09:05:35 +0200

    Bump default LuaRocks version to 2.4.2

--- rockz.plugin.zsh
+++ rockz.plugin.zsh
@@ -7,7 +7,7 @@
 #
 
 : ${ROCKZ_HOME:=${HOME}/.rockenvs}
-: ${ROCKZ_LUAROCKS_VERSION:=2.4.1}
+: ${ROCKZ_LUAROCKS_VERSION:=2.4.2}
 : ${ROCKZ_LUAROCKS_DISTURL:='https://luarocks.org/releases/'}
 
 typeset -gr _rockz_dir=${0:A:h}
