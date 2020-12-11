59d8018dd74ba075854325c7d2355d2201cf2229
Adrian Perez de Castro <aperez@igalia.com>
2020-12-12 00:27:10 +0200

    Bump default LuaRocks version to 3.5.0

--- rockz.plugin.zsh
+++ rockz.plugin.zsh
@@ -7,7 +7,7 @@
 #
 
 : ${ROCKZ_HOME:=${HOME}/.rockenvs}
-: ${ROCKZ_LUAROCKS_VERSION:=3.3.1}
+: ${ROCKZ_LUAROCKS_VERSION:=3.5.0}
 : ${ROCKZ_LUAROCKS_DISTURL:='https://luarocks.org/releases/'}
 
 typeset -gr _rockz_dir=${0:A:h}
